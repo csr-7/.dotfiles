b44d3fb781b6c5ba16ec84b1beda874d30f1f5b2
Chris <christophersruiz7@gmail.com>
2024-10-08 12:09:57 -0700

    nvim from scratch

---
+++ configs/.config/nvim/lua/core/options.lua
@@ -0,0 +1,21 @@
+-- General Options --
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+
+
+-- Set defautlt tab
+vim.opt.tabstop = 4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
+vim.expandtab = True
+
+
+-- Better editor UI
+vim.opt.number = true
+vim.opt.relativenumber = false
+--vim.opt.signcolumn = 'yes'
+--vim.opt.cursorline = false
+--vim.opt.colorcolumn = '80,120'
+--
+--
